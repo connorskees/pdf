937c184bbc6dd18465cace8d3570618265e89ec9
Connor Skees <connor1skees@gmail.com>
2021-01-01 21:33:30 -0500

    add tests for date parsing

--- src/date.rs
+++ src/date.rs
@@ -3,7 +3,7 @@ use crate::{
     NUMBERS,
 };
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Date {
     year: Option<u16>,
     month: Option<u16>,
@@ -19,7 +19,7 @@ pub struct Date {
 
 impl Date {
     pub(crate) fn from_str(s: &str) -> PdfResult<Self> {
-        let mut chars = s.bytes();
+        let mut chars = s.bytes().peekable();
 
         let mut date = Date {
             year: None,
@@ -61,7 +61,7 @@ impl Date {
                     let next = match chars.next() {
                         Some(n @ b'0'..=b'9') => n - b'0',
                         None => return Ok(date),
-                        found => {
+                        found @ Some(..) => {
                             return Err(ParseError::MismatchedByteMany {
                                 expected: NUMBERS,
                                 found,
@@ -84,6 +84,19 @@ impl Date {
         unit!(minute, 2);
         unit!(second, 2);
         date.ut_relationship = chars.next().map(UtRelationship::from_byte).transpose()?;
+        match chars.peek().cloned() {
+            Some(b'\'') => {
+                chars.next();
+
+                if !chars.peek().is_none() {
+                    return Err(ParseError::MismatchedByte {
+                        expected: b'\'',
+                        found: chars.next(),
+                    });
+                }
+            }
+            _ => {}
+        }
         unit!(ut_hour_offset, 2);
         match chars.next() {
             Some(b'\'') => {}
@@ -95,12 +108,21 @@ impl Date {
             }
         }
         unit!(ut_minute_offset, 2);
+        match chars.next() {
+            Some(b'\'') | None => {}
+            found => {
+                return Err(ParseError::MismatchedByte {
+                    expected: b'\'',
+                    found,
+                })
+            }
+        }
 
         Ok(date)
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum UtRelationship {
     Plus,
     Minus,
@@ -120,5 +142,65 @@ impl UtRelationship {
                 })
             }
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{Date, UtRelationship};
+
+    #[test]
+    /// Test case taken from a Libre Office pdf
+    fn ends_with_single_quote_no_ut_hour() {
+        assert_eq!(
+            Date {
+                year: Some(2020),
+                month: Some(11),
+                day: Some(25),
+                hour: Some(2),
+                minute: Some(11),
+                second: Some(08),
+                ut_relationship: Some(UtRelationship::Equal),
+                ut_hour_offset: None,
+                ut_minute_offset: None,
+            },
+            Date::from_str("D:20201125021108Z'").unwrap()
+        )
+    }
+
+    #[test]
+    fn ut_min_and_ut_hour_set() {
+        assert_eq!(
+            Date {
+                year: Some(2020),
+                month: Some(12),
+                day: Some(3),
+                hour: Some(18),
+                minute: Some(48),
+                second: Some(27),
+                ut_relationship: Some(UtRelationship::Minus),
+                ut_hour_offset: Some(8),
+                ut_minute_offset: Some(0),
+            },
+            Date::from_str("D:20201203184827-08'00'").unwrap()
+        )
+    }
+
+    #[test]
+    fn no_ut_set() {
+        assert_eq!(
+            Date {
+                year: Some(2008),
+                month: Some(6),
+                day: Some(11),
+                hour: Some(16),
+                minute: Some(56),
+                second: Some(3),
+                ut_relationship: None,
+                ut_hour_offset: None,
+                ut_minute_offset: None,
+            },
+            Date::from_str("D:20080611165603").unwrap()
+        )
     }
 }
