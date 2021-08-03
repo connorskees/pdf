95fdc5105e990a2ea963d6835349b2dd9c8fb0a8
Connor Skees <connor1skees@gmail.com>
2021-08-03 19:28:01 -0400

    support dct decompression

---
+++ src/filter/dct.rs
@@ -0,0 +1,20 @@
+use std::{
+    borrow::Cow,
+    io::{BufReader, Cursor},
+};
+
+pub struct DctDecoder<'a> {
+    buffer: Cow<'a, [u8]>,
+}
+
+impl<'a> DctDecoder<'a> {
+    pub fn new(buffer: Cow<'a, [u8]>) -> Self {
+        Self { buffer }
+    }
+
+    pub fn decode(self) -> Vec<u8> {
+        let mut decoder = jpeg_decoder::Decoder::new(BufReader::new(Cursor::new(self.buffer)));
+
+        decoder.decode().unwrap()
+    }
+}
