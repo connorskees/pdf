d9b167274eba7fbccaca0a0fdd6583d1ed99fe6b
Connor Skees <connor1skees@gmail.com>
2021-08-05 11:34:34 -0400

    reformat viewer prefs docs

--- src/viewer_preferences.rs
+++ src/viewer_preferences.rs
@@ -7,94 +7,118 @@ use crate::{
 
 #[derive(Debug)]
 pub struct ViewerPreferences {
-    /// A flag specifying whether to hide the conforming reader's tool bars when the document is active.
+    /// A flag specifying whether to hide the conforming reader's tool bars when
+    /// the document is active.
     ///
     /// Default value: false
     hide_toolbar: bool,
 
-    /// A flag specifying whether to hide the conforming reader's menu bar when the document is active.
+    /// A flag specifying whether to hide the conforming reader's menu bar when
+    /// the document is active.
     ///
     /// Default value: false
     hide_menubar: bool,
 
-    /// A flag specifying whether to hide user interface elements in the document's window (such as scroll
-    /// bars and navigation controls), leaving only the document's contents displayed.
+    /// A flag specifying whether to hide user interface elements in the document's
+    /// window (such as scroll bars and navigation controls), leaving only the
+    /// document's contents displayed.
     ///
     /// Default value: false
     hide_window_ui: bool,
 
-    /// A flag specifying whether to resize the document's window to fit the size of the first displayed page.
+    /// A flag specifying whether to resize the document's window to fit the size
+    /// of the first displayed page.
     ///
     /// Default value: false
     fit_window: bool,
 
-    /// A flag specifying whether to position the document's window in the center of the screen.
+    /// A flag specifying whether to position the document's window in the center
+    /// of the screen.
     ///
     /// Default value: false
     center_window: bool,
 
-    /// A flag specifying whether the window's title bar should display the document title taken from the Title
-    /// entry of the document information dictionary. If false, the title bar should instead display the name
-    /// of the PDF file containing the document.
+    /// A flag specifying whether the window's title bar should display the document
+    /// title taken from the Title entry of the document information dictionary.
+    /// If false, the title bar should instead display the name of the PDF file
+    /// containing the document.
     ///
     /// Default value: false
     display_doc_title: bool,
 
-    /// The document's page mode, specifying how to display the document on exiting full-screen mode
+    /// The document's page mode, specifying how to display the document on exiting
+    /// full-screen mode
     ///
-    /// This entry is meaningful only if the value of the PageMode entry in the Catalog dictionary is FullScreen;
-    /// it shall be ignored otherwise. Default value: UseNone.
+    /// This entry is meaningful only if the value of the PageMode entry in the
+    /// Catalog dictionary is FullScreen;
+    /// it shall be ignored otherwise.
+    ///
+    /// Default value: UseNone.
     non_full_screen_page_mode: PageMode,
 
     /// The predominant reading order for text
     ///
-    /// This entry has no direct effect on the document's contents or page numbering but may be used to determine
-    /// the relative positioning of pages when displayed side by side or printed n-up.
+    /// This entry has no direct effect on the document's contents or page numbering
+    /// but may be used to determine the relative positioning of pages when displayed
+    /// side by side or printed n-up.
     ///
     /// Default value: L2R
     direction: TextDirection,
 
-    /// The name of the page boundary representing the area of a page that shall be displayed when viewing the
-    /// document on the screen. The value is the key designating the relevant page boundary in the page object.
-    /// If the specified page boundary is not defined in the page object, its default value shall be used.
+    /// The name of the page boundary representing the area of a page that shall
+    /// be displayed when viewing the document on the screen. The value is the
+    /// key designating the relevant page boundary in the page object. If the
+    /// specified page boundary is not defined in the page object, its default
+    /// value shall be used.
     ///
     /// Default value: CropBox.
     ///
-    /// This entry is intended primarily for use by prepress applications that interpret or manipulate the page
-    /// boundaries. Most conforming readers disregard it
+    /// This entry is intended primarily for use by prepress applications that
+    /// interpret or manipulate the page boundaries. Most conforming readers
+    /// disregard it
     view_area: PageBoundary,
 
-    /// The name of the page boundary to which the contents of a page shall be clipped when viewing the document
-    /// on the screen. The value is the key designating the relevant page boundary in the page object. If the
-    /// specified page boundary is not defined in the page object, its default value shall be used
+    /// The name of the page boundary to which the contents of a page shall be
+    /// clipped when viewing the document on the screen. The value is the key
+    /// designating the relevant page boundary in the page object. If the specified
+    /// page boundary is not defined in the page object, its default value shall
+    /// be used
     ///
     /// Default value: CropBox
     ///
-    /// This entry is intended primarily for use by prepress applications that interpret or manipulate the page
-    /// boundaries. Most conforming readers disregard it
+    /// This entry is intended primarily for use by prepress applications that
+    /// interpret or manipulate the page boundaries. Most conforming readers
+    /// disregard it
     view_clip: PageBoundary,
 
-    /// The name of the page boundary representing the area of a page that shall be rendered when printing the
-    /// document. The value is the key designating the relevant page boundary in the page object. If the specified
-    /// page boundary is not defined in the page object, its default value shall be used
+    /// The name of the page boundary representing the area of a page that shall
+    /// be rendered when printing the document. The value is the key designating
+    /// the relevant page boundary in the page object. If the specified page
+    /// boundary is not defined in the page object, its default value shall be
+    /// used
     ///
     /// Default value: CropBox
     ///
-    /// This entry is intended primarily for use by prepress applications that interpret or manipulate the page
-    /// boundaries. Most conforming readers disregard it
+    /// This entry is intended primarily for use by prepress applications that
+    /// interpret or manipulate the page boundaries. Most conforming readers
+    /// disregard it
     print_area: PageBoundary,
 
-    /// The name of the page boundary to which the contents of a page shall be clipped when printing the document.
-    /// The value is the key designating the relevant page boundary in the page object. If the specified page
-    /// boundary is not defined in the page object, its default value shall be used
+    /// The name of the page boundary to which the contents of a page shall be
+    /// clipped when printing the document. The value is the key designating the
+    /// relevant page boundary in the page object. If the specified page boundary
+    /// is not defined in the page object, its default value shall be used
     ///
     /// Default value: CropBox
     ///
-    /// This entry is intended primarily for use by prepress applications that interpret or manipulate the page
-    /// boundaries. Most conforming readers disregard it
+    /// This entry is intended primarily for use by prepress applications that
+    /// interpret or manipulate the page boundaries. Most conforming readers
+    /// disregard it
     print_clip: PageBoundary,
 
-    /// The page scaling option that shall be selected when a print dialog is displayed for this document.
+    /// The page scaling option that shall be selected when a print dialog is
+    /// displayed for this document.
+    ///
     /// Valid values are
     ///   * None, which indicates no page scaling
     ///   * AppDefault, which indicates the conforming reader's default print scaling.
@@ -103,31 +127,37 @@ pub struct ViewerPreferences {
     ///
     /// Default value: AppDefault.
     ///
-    /// If the print dialog is suppressed and its parameters are provided from some other source, this entry
-    /// nevertheless shall be honored
+    /// If the print dialog is suppressed and its parameters are provided from
+    /// some other source, this entry nevertheless shall be honored
     print_scaling: PageScaling,
 
-    /// The paper handling option that shall be used when printing the file from the print dialog
+    /// The paper handling option that shall be used when printing the file from
+    /// the print dialog
     duplex: Option<Duplex>,
 
-    /// A flag specifying whether the PDF page size shall be used to select the input paper tray. This setting
-    /// influences only the preset values used to populate the print dialog presented by a conforming reader. If
-    /// PickTrayByPDFSize is true, the check box in the print dialog associated with input paper tray shall be checked.
+    /// A flag specifying whether the PDF page size shall be used to select the
+    /// input paper tray. This setting influences only the preset values used
+    /// to populate the print dialog presented by a conforming reader. If
+    /// PickTrayByPDFSize is true, the check box in the print dialog associated
+    /// with input paper tray shall be checked.
     ///
-    /// This setting has no effect on operating systems that do not provide the ability to pick the input tray by size.
+    /// This setting has no effect on operating systems that do not provide the
+    /// ability to pick the input tray by size.
     ///
     /// Default value: as defined by the conforming reader
     pick_tray_by_pdf_size: bool,
 
-    /// The page numbers used to initialize the print dialog box when the file is printed. The array shall contain an
-    /// even number of integers to be interpreted in pairs, with each pair specifying the first and last pages in a
-    /// sub-range of pages to be printed.The first page of the PDF file shall be denoted by 1.
+    /// The page numbers used to initialize the print dialog box when the file
+    /// is printed. The array shall contain an even number of integers to be
+    /// interpreted in pairs, with each pair specifying the first and last pages
+    /// in a sub-range of pages to be printed.The first page of the PDF file shall
+    /// be denoted by 1.
     ///
     /// Default value: as defined by the conforming reader
     print_page_range: Option<Vec<PageRange>>,
 
-    /// The number of copies that shall be printed when the print dialog is opened for this file. Values outside this
-    /// range shall be ignored.
+    /// The number of copies that shall be printed when the print dialog is opened
+    /// for this file. Values outside this range shall be ignored.
     ///
     /// Default value: as defined by the conforming reader, but typically 1
     num_copies: Option<u32>,
@@ -254,7 +284,8 @@ pdf_enum!(
     enum TextDirection {
         LeftToRight = "L2R",
 
-        /// Right to left (including vertical writing systems, such as Chinese, Japanese, and Korean)
+        /// Right to left (including vertical writing systems, such as Chinese,
+        /// Japanese, and Korean)
         RightToLeft = "R2L",
     }
 );
