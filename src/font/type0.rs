b5dbe646dfb9218003375084e094ce718df5c875
Connor Skees <connor1skees@gmail.com>
2023-06-09 16:06:42 +0000

    add base font dict to type0 fonts

--- src/font/type0.rs
+++ src/font/type0.rs
@@ -1,11 +1,13 @@
 use crate::{
     error::{ParseError, PdfResult},
-    objects::{Name, Object, ObjectType},
+    objects::{Object, ObjectType},
     stream::Stream,
     FromObj, Resolve,
 };
 
-use super::{cid::CidFontDictionary, cjk::PredefinedCjkCmapName, cmap::ToUnicodeCmapStream};
+use super::{
+    cid::CidFontDictionary, cjk::PredefinedCjkCmapName, cmap::ToUnicodeCmapStream, BaseFontDict,
+};
 
 #[derive(Debug)]
 enum Type0FontEncoding<'a> {
@@ -29,8 +31,10 @@ impl<'a> FromObj<'a> for Type0FontEncoding<'a> {
 /// from a fontlike object called a CIDFont. A composite font shall be represented
 /// by a font dictionary whose Subtype value is Type0. The Type 0 font is known
 /// as the root font, and its associated CIDFont is called its descendant.
-#[derive(Debug, FromObj)]
+#[derive(Debug)]
 pub struct Type0Font<'a> {
+    base: BaseFontDict<'a>,
+
     /// The name of the font. If the descendant is a Type 0 CIDFont, this
     /// name should be the concatenation of the CIDFont’s BaseFont name,
     /// a hyphen, and the CMap name given in the Encoding entry (or the
@@ -41,21 +45,37 @@ pub struct Type0Font<'a> {
     ///       font program associated directly with a Type 0 font dictionary.
     ///       The conventions described here ensure maximum compatibility with
     ///       existing readers
-    #[field("BaseFont")]
-    base_font: Name,
+    base_font: String,
 
     /// The name of a predefined CMap, or a stream containing a CMap that
     /// maps character codes to font numbers and CIDs. If the descendant is
     /// a Type 2 CIDFont whose associated TrueType font program is not embedded
     /// in the PDF file, the Encoding entry shall be a predefined CMap name
-    #[field("Encoding")]
     encoding: Type0FontEncoding<'a>,
 
-    /// A one-element array specifying the CIDFont dictionary that is the descendant of this Type 0 font
-    #[field("DescendantFonts")]
+    /// A one-element array specifying the CIDFont dictionary that is the descendant
+    /// of this Type 0 font
     descendant_fonts: [CidFontDictionary<'a>; 1],
 
     /// A stream containing a CMap file that maps character codes to Unicode values
-    #[field("ToUnicode")]
     to_unicode: Option<ToUnicodeCmapStream<'a>>,
+}
+
+impl<'a> FromObj<'a> for Type0Font<'a> {
+    fn from_obj(obj: Object<'a>, resolver: &mut dyn Resolve<'a>) -> PdfResult<Self> {
+        let mut dict = resolver.assert_dict(obj)?;
+        let base = BaseFontDict::from_dict(&mut dict, resolver)?;
+        let base_font = dict.expect_name("BaseFont", resolver)?;
+        let encoding = dict.expect("Encoding", resolver)?;
+        let descendant_fonts = dict.expect("DescendantFonts", resolver)?;
+        let to_unicode = dict.get("ToUnicode", resolver)?;
+
+        Ok(Self {
+            base,
+            base_font,
+            encoding,
+            descendant_fonts,
+            to_unicode,
+        })
+    }
 }
