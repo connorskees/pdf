417712fbeaa2c4ce0c3b56bc13a1e7f5f5e16411
Connor Skees <connor1skees@gmail.com>
2023-06-09 07:34:31 +0000

    initial pass at aesv2 decryption

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,8 +1,18 @@
+#![allow(warnings)]
+
 use std::collections::HashMap;
 
+use aes::cipher::{
+    block_padding::{NoPadding, Pkcs7},
+    generic_array::GenericArray,
+    BlockDecrypt, BlockDecryptMut, KeyInit, KeyIvInit,
+};
+
 use crate::{
-    objects::{Dictionary, Name, Object},
+    file_specification::FileIdentifier,
+    objects::{Dictionary, Name, Object, Reference},
     resolve::Resolve,
+    stream::Stream,
     FromObj, PdfResult,
 };
 
@@ -85,6 +95,43 @@ pub struct Encryption<'a> {
     #[field("EFF", default = Name("Identity".to_owned()))]
     embedded_file_filter: Name,
 
+    // todo: below fields should only be in standard security handler, not all
+    /// A number specifying which revision of the standard security handler shall
+    /// be used to interpret this dictionary
+    ///
+    /// 2 => if the document is encrypted with a V value less than 2 and does not
+    /// have any of the access permissions set to 0 (by means of the P
+    /// entry, below) that are designated “Security handlers of revision 3
+    /// or greater”
+    ///
+    /// 3 => if the document is encrypted with a V value of 2 or 3, or has any
+    /// “Security handlers of revision 3 or greater” access permissions set
+    /// to 0
+    ///
+    /// 4 => if the document is encrypted with a V value of 4
+    #[field("R")]
+    revision_number: i32,
+
+    /// A 32-byte string, based on both the owner and user passwords, that shall
+    /// be used in computing the encryption key and in determining whether
+    /// a valid owner password was entered
+    #[field("O")]
+    owner: String,
+
+    /// A set of flags specifying which operations shall be permitted when the
+    /// document is opened with user access
+    #[field("U")]
+    user: String,
+
+    /// A set of flags specifying which operations shall be permitted when the
+    /// document is opened with user access
+    #[field("P")]
+    user_permission_flags: UserAccessPermissions,
+
+    /// Indicates whether the document-level metadata stream shall be encrypted
+    #[field("EncryptMetadata", default = true)]
+    encrypt_metadata: bool,
+
     // todo: additional fields here
     #[field]
     other: Dictionary<'a>,
@@ -123,8 +170,8 @@ pub struct CryptFilter {
     /// that use the same crypt filter. Therefore, there shall be a
     /// one-to-one relationship between a crypt filter name and the
     /// corresponding encryption key.
-    #[field("CFM")]
-    crypt_filter_method: Option<CryptFilterMethod>,
+    #[field("CFM", default = CryptFilterMethod::default())]
+    crypt_filter_method: CryptFilterMethod,
 
     /// The event to be used to trigger the authorization that is required to
     /// access encryption keys used by this filter. If authorization fails,
@@ -175,10 +222,116 @@ enum CryptFilterMethod {
     AesV2 = "AESV2",
 }
 
-struct UserAccessPermissions(u32);
+#[derive(Debug, Copy, Clone)]
+struct UserAccessPermissions(i32);
 
 impl<'a> FromObj<'a> for UserAccessPermissions {
     fn from_obj(obj: Object<'a>, resolver: &mut dyn Resolve<'a>) -> PdfResult<Self> {
-        Ok(Self(resolver.assert_unsigned_integer(obj)?))
+        Ok(Self(resolver.assert_integer(obj)?))
+    }
+}
+
+const PADDING: [u8; 32] = [
+    0x28, 0xBF, 0x4E, 0x5E, 0x4E, 0x75, 0x8A, 0x41, 0x64, 0x00, 0x4E, 0x56, 0xFF, 0xFA, 0x01, 0x08,
+    0x2E, 0x2E, 0x00, 0xB6, 0xD0, 0x68, 0x3E, 0x80, 0x2F, 0x0C, 0xA9, 0xFE, 0x64, 0x53, 0x69, 0x7A,
+];
+
+/// Manages encryption for entire document
+pub struct SecurityHandler<'a> {
+    encryption: Encryption<'a>,
+    file_identifier: FileIdentifier,
+}
+
+impl<'a> SecurityHandler<'a> {
+    pub fn new(encryption: Encryption<'a>, file_identifier: FileIdentifier) -> Self {
+        assert_eq!(encryption.length % 8, 0);
+
+        Self {
+            encryption,
+            file_identifier,
+        }
+    }
+
+    fn compute_encryption_key(&self, password: &[u8]) -> Vec<u8> {
+        let padded_password = if password.len() >= 32 {
+            password[..32].to_owned()
+        } else {
+            let mut b = password.to_owned();
+            b.extend_from_slice(&PADDING[..32 - password.len()]);
+            b
+        };
+
+        assert_eq!(padded_password.len(), 32);
+
+        let mut hash = md5::Context::new();
+
+        hash.consume(&padded_password);
+        hash.consume(&self.encryption.owner.as_bytes());
+        hash.consume(&self.encryption.user_permission_flags.0.to_le_bytes());
+        hash.consume(&self.file_identifier.0[0].as_bytes());
+
+        if self.encryption.revision_number >= 4 && !self.encryption.encrypt_metadata {
+            hash.consume(&[0xFF, 0xFF, 0xFF, 0xFF]);
+        }
+
+        let mut hash = hash.compute();
+
+        if self.encryption.revision_number >= 3 {
+            let n = self.encryption.length / 8;
+            for _ in 0..50 {
+                hash = md5::compute(&hash[..n as usize]);
+            }
+        }
+
+        let n = if self.encryption.revision_number == 2 {
+            5
+        } else {
+            self.encryption.length / 8
+        };
+
+        hash[..n as usize].to_vec()
+    }
+
+    pub fn decrypt_string(&mut self, s: Vec<u8>) -> PdfResult<String> {
+        todo!()
+    }
+
+    pub fn decrypt_stream(&self, mut stream: Vec<u8>, reference: Reference) -> PdfResult<Vec<u8>> {
+        let filter_name = &self.encryption.stream_filter.0;
+        let filter = self
+            .encryption
+            .crypt_filters
+            .as_ref()
+            .unwrap()
+            .get(filter_name)
+            .unwrap();
+
+        assert_eq!(filter.crypt_filter_method, CryptFilterMethod::AesV2);
+
+        dbg!(&self.encryption);
+
+        let mut key = self.compute_encryption_key(&[]);
+        let key_len = key.len();
+
+        key.extend_from_slice(&reference.object_number.to_le_bytes()[..3]);
+        key.extend_from_slice(&reference.generation.to_le_bytes()[..2]);
+        key.extend_from_slice(b"sAlT");
+
+        let key = &md5::compute(&key)[..key_len];
+
+        let mut dec = aes::Aes128Dec::new_from_slice(&key).unwrap();
+
+        let key = &[0; 32];
+
+        assert_eq!(stream.len() % 16, 0);
+
+        type Aes128CbcDec = cbc::Decryptor<aes::Aes128>;
+
+        let (iv, ciphertext) = stream.split_at_mut(16);
+        let cipher = Aes128CbcDec::new_from_slices(key, iv).unwrap();
+
+        cipher.decrypt_padded_mut::<Pkcs7>(ciphertext).unwrap();
+
+        Ok(stream)
     }
 }
