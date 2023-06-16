373589c4e7a756d4e9f40e77dec4ded9c70da314
Connor Skees <connor1skees@gmail.com>
2023-06-16 02:33:22 +0000

    rectangle helper method for conversion to path

--- src/data_structures/rectangle.rs
+++ src/data_structures/rectangle.rs
@@ -1,4 +1,10 @@
-use crate::{catalog::assert_len, error::PdfResult, objects::Object, FromObj, Resolve};
+use crate::{
+    catalog::assert_len,
+    error::PdfResult,
+    geometry::{path_builder::PathBuilder, Path},
+    objects::Object,
+    FromObj, Resolve,
+};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Rectangle {
@@ -35,5 +41,15 @@ impl Rectangle {
 
     pub fn height(&self) -> f32 {
         self.upper_right_y - self.lower_left_y
+    }
+
+    pub fn as_path(&self) -> Path {
+        let mut path_builder = PathBuilder::new();
+        path_builder.relative_move_to(self.lower_left_x, self.lower_left_y);
+        path_builder.relative_line_to(0.0, self.upper_right_y);
+        path_builder.relative_line_to(self.upper_right_x, 0.0);
+        path_builder.relative_line_to(0.0, -self.upper_right_y);
+        path_builder.close_path();
+        path_builder.outline.paths[0].clone()
     }
 }
